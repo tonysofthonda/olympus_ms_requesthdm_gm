72f831647c2e175cdd9715441afefb0c2f378dec
anttoniobh <abhernandez@stefanini.com>
2023-05-25 10:48:58 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Request HDM GM Microservice [ms.requesthdm_gm]  
+El propósito de este microservicio es enviar las fixed Orders a la API de MaxTransit.
+
---
+++ src/main/java/com/honda/olympus/ms/requesthdm_gm/Application.java
@@ -0,0 +1,25 @@
+package com.honda.olympus.ms.requesthdm_gm;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application 
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault( TimeZone.getTimeZone("America/Mexico_City") );
+	}
+
+}
