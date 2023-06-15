453a1e2b60561849a7c0a6f8f7b4fe9103b6114c
vzpintor <ovpintor@stefanini.com>
2023-06-15 11:24:16 -0600

    configured profiles for environments

--- src/main/java/com/honda/olympus/ms/requesthdm_gm/Application.java
+++ src/main/java/com/honda/olympus/ms/requesthdm_gm/Application.java
@@ -1,27 +1,55 @@
 package com.honda.olympus.ms.requesthdm_gm;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import com.honda.olympus.ms.requesthdm_gm.config.ApplicationProperties;
+import com.honda.olympus.ms.requesthdm_gm.config.DefaultProfileUtil;
+import com.honda.olympus.ms.requesthdm_gm.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableScheduling
 @SpringBootApplication
-public class Application 
-{
+@EnableConfigurationProperties({ApplicationProperties.class})
+public class Application implements InitializingBean {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault( TimeZone.getTimeZone("America/Mexico_City") );
-	}
-	
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    private final Environment env;
+
+    public Application(Environment env) {
+        this.env = env;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) &&
+                activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+            log.error("You have misconfigured your application! It should not run with both the 'dev' and 'prod' profiles at the same time.");
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
+    }
+
 }
---
+++ src/main/java/com/honda/olympus/ms/requesthdm_gm/util/Constants.java
@@ -0,0 +1,12 @@
+package com.honda.olympus.ms.requesthdm_gm.util;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    //Environments
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_QA = "qa";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+}
